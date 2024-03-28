f1a62d6d0635a3720a3de011b30d4a97417a0321
Mazzie <mazziechai@gmail.com>
2024-03-28 10:50:05 -0700

    docs: document io

--- src/io.rs
+++ src/io.rs
@@ -1,8 +1,24 @@
+//! Input/output utilities.
+
+/// Aphelion uses a port-based I/O system with a theoretical maximum of 65,536 ports available.
+/// There are four internal ports defined in the Aphelion ISA:
+///
+/// 0. [Interrupt controller](Port::INT): Manages interrupts and the Interrupt Vector Table.
+/// 1. [Input/output controller](Port::IO): Manages I/O operations and provides I/O information.
+/// 2. [Memory management unit](Port::MMU): Oversees memory access and address translation.
+/// 3. [System timer](Port::SYSTIMER): Provides time information and can be used as a PIT/HPET.
+///
+/// These ports are given equivalent constants within the struct for ease of use.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Port(pub u16);
+
 impl Port {
+    /// Interrupt controller.
     pub const INT: Self = Self(0);
+    /// Input/output controller.
     pub const IO: Self = Self(1);
+    /// Memory management unit.
     pub const MMU: Self = Self(2);
+    /// System timer.
     pub const SYSTIMER: Self = Self(3);
 }
